03f0a4745434cc69239b9fa9b2f0bba47412c563
quentalt <quentin.altieri@gmail.com>
2024-10-07 16:10:48 +0200

    petit ménage

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+
 require('dotenv').config()
 
 app.use(express.json())
@@ -12,6 +11,5 @@ const animeRouter = require("./routes/anime.router")
 
 app.use("/manga", mangaRouter)
 app.use("/anime", animeRouter)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
