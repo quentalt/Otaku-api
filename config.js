5f8f539628b00c881cb15276ab0e9d799ccd695d
quentalt <quentin.altieri@gmail.com>
2024-10-07 14:30:43 +0200

    add pg module

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const pg = require('pg');
 
 module.exports = {
     development: {
         use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
+        dialectModule: pg,
         dialectOptions: {
             ssl: {
                 require: true,
